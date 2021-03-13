e000de4f7938534c29d8dd0cfa44547a9f102527
hyperspin <spin.half@gmail.com>
2021-03-13 18:05:39 -0500

    updated comments.

--- cachematrix.R
+++ cachematrix.R
@@ -5,10 +5,10 @@
 #The function, `makecacheMatrix` creates a special "matrix", which is
 #really a list containing a function to
 
-#1.  set the value of the matrix
-#$2.  get the value of the matrix
-#3.  set the value of the inverse of teh matrix
-#4.  get the value of the inverse of teh matrix
+#1.  set: function to set the value of the matrix
+#2.  get: function to get the value of the matrix
+#3.  setsolve: function to set the  inverse of the matrix
+#4.  getsolve: function to get the  inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -25,11 +25,11 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
-
-
-## Write a short comment describing this function
-#inverts the matrix if its a new matrix, or teh solution is not available.
-# if matrix didn't change, uses the cached solution.
+## 
+#inverts the matrix if its a new matrix.
+# calls getsolve function.  uses the cached solution if the rturn value (m) is not NULL
+#Prints the message "setting cached data.
+# if m is NULL, sets data to the new matrix and computes the inverse..
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
